10b4e6bff23a43a54c84a904174eab18fde17627
fducha <fducha@list.ru>
2018-02-06 23:07:34 +0300

    2-15 рубеж пройден

--- step2-15.py
+++ step2-15.py
@@ -3,34 +3,37 @@ matrix = [[0 for i in range(num)] for j in range(num)]
 value = 0
 n = num
 offset = 0
-while n > 0:
+while n >= 0:
     # up side
-    for i in range(n):
+    for i in range(n - 1):
         value += 1
         matrix[offset][i + offset] = value
-        print(value, end='\t')
-    print()
+    #     print(value, end='\t')
+    # print()
     # right side
     for i in range(n - 1):
         value += 1
-        matrix[i + offset + 1][num - offset - 1] = value
-        print(value, end='\t')
-    print()
+        matrix[i + offset][num - offset - 1] = value
+    #     print(value, end='\t')
+    # print()
     # bottom side
-    for i in range(n - 2, -1, -1):
+    for i in range(n - 1, 0, -1):
         value += 1
-        # matrix[num - 1 - offset][i + 1] = value
-        print(value, end='\t')
-    print()
+        matrix[num - 1 - offset][i + offset] = value
+    #     print(value, end='\t')
+    # print()
     # left side
-    for i in range(n - 2, 0, -1):
+    for i in range(n - 1, 0, -1):
         value += 1
-        # matrix[i][offset] = value
-        print(value, end='\t')
-    print()
+        matrix[i + offset][offset] = value
+    #     print(value, end='\t')
+    # print()
     n -= 2
     offset += 1
 
+if num % 2 != 0:
+    matrix[num // 2][num // 2] = num * num
+
 for row in matrix:
-    [print(i, end='\t') for i in row]
+    [print(i, end=' ') for i in row]
     print()
