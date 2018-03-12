9a9fb94170c9f1e25249bd12efe5e9de1401293c
fducha <fducha@list.ru>
2018-03-12 15:21:36 +0300

    step 6 is done

--- stepic/Algorhythms/step6.py
+++ stepic/Algorhythms/step6.py
@@ -1,3 +1,5 @@
+from random import randint
+
 class Segment:
     def __init__(self, start, end):
         self.start = start
@@ -11,27 +13,32 @@ class Segment:
         return 'Segment: {} - {}'.format(self.start, self.end)
 
 
-def is_point_right(point: int, segments: list) -> bool:
-    for s in filter(lambda s: s.marked, segments):
-        if point == s.end:
-            return True
-    return False
-
-
-def my_sort(lst: list):
-    for i in range(len(lst)):
-        for j in range(len(lst)):
-            pass
+# def my_sort(lst: list):
+#     for i in range(len(lst) - 1):
+#         for j in range(i + 1, len(lst)):
+#             if lst[i].end > lst[j].end:
+#                 lst[i], lst[j] = lst[j], lst[i]
+#             elif lst[i].end == lst[j].end:
+#                 if lst[i].start > lst[j].start:
+#                     lst[i], lst[j] = lst[j], lst[i]
 
 
 segments = []
 for _ in range(int(input())):
     a, b = map(int, input().split())
+    # a = randint(0, 100)
+    # b = randint(a, 100)
+    if a > b:
+        a, b = b, a
     segments.append(Segment(a, b))
 
 min_points = set()
-for seg in sorted(segments, key=lambda seg: seg.end):
-    if is_point_right(seg.end, segments):
+print(segments)
+segments.sort(key=lambda s: s.end)
+print('Sorted', segments)
+
+for seg in segments:
+    if seg.marked:
         continue
     min_points.add(seg.end)
     seg.marked = True
@@ -43,10 +50,10 @@ print(len(min_points))
 print(*min_points)
 
 
-for p in min_points:
-    cont = False
-    for seg in segments:
-        if p in seg:
-            cont = True
+for s in segments:
+    has_dot = False
+    for dot in min_points:
+        if dot in s:
+            has_dot = True
             break
-    print(p, 'OK' if cont else 'FAIL !!!')
+    print(s, 'test', 'OK, dot {}'.format(dot) if has_dot else 'FAIL!!!!!!!')
