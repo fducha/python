c131978d09edbdcbc15872ff0baaa3db5f26b0b3
fducha <fducha@list.ru>
2018-03-16 03:35:41 +0300

    step 9

--- stepic/Algorhythms/step9.py
+++ stepic/Algorhythms/step9.py
@@ -1,28 +1,7 @@
-class LetterString:
-    def __init__(self, string):
-        self.string = string[:]
-        self.char_frequency = {}
-        for s in set(self.string):
-            self.char_frequency.setdefault(s, 0)
-            self.char_frequency[s] = self.string.count(s)
-        self.char_frequency = sorted(self.char_frequency.items(), key=lambda x: x[1])
-        self.char_code = {}
-
-    def count(self):
-        return len(set(self.string))
-
-
-class HaffmanCode:
-    def __init__(self, ls: LetterString):
-        self.lstring = ls
-        self.encode()
-
-    def encode(self):
-        pass
-
-    def __repr__(self):
-        return self.lstring.string
-
-
-code = HaffmanCode(LetterString(input()))
-print(code)
+input_str = input()
+frequency = {}
+for s in set(input_str):
+    frequency.setdefault(s, 0)
+    frequency[s] = input_str.count(s)
+# frequency = dict(sorted(frequency.items(), key=lambda x: x[1]))
+print(frequency)
