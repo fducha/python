8486a8c1c90dc2ad81eaf6867a5ee6ebf59d47aa
fducha <fducha@list.ru>
2018-03-18 01:24:19 +0300

    step 9 node

--- stepic/Algorhythms/step9.py
+++ stepic/Algorhythms/step9.py
@@ -5,27 +5,41 @@ class Char:
 
 
 class Node:
-    def __init__(self, value, parent=None):
+    def __init__(self, value=None, frequency=0, parent=None):
         self.value = value
+        self.frequency = frequency
         self.left = None
         self.right = None
         self.parent = parent
+
+    def set_children(self, left, right):
+        self.left = left
+        left.parent = self
+        self.right = right
+        right.parent = self
+        self.frequency = left.frequency + right.frequency
+
+    def has_children(self):
+        return self.left is not None and self.right is not None
+
+    def __repr__(self):
+        if not self.has_children():
+            return '<Node {} ({})>'.format(self.value, self.frequency)
+        return '<Node {} ({}): left {}, right {}>'.format(self.value, self.frequency, self.left, self.right)
 
 
 input_str = input()
 frequencies = []
 for s in set(input_str):
-    frequencies.append(Char(s, input_str.count(s)))
+    frequencies.append(Node(value=s, frequency=input_str.count(s)))
 if len(frequencies) < 2:
     print(1, len(input_str))
-    print('{}: 0'.format(frequencies[0].chr))
+    print('{}: 0'.format(frequencies[0].value))
     print('0' * len(input_str))
 else:
-    frequencies.sort(key=lambda c: c.frequency)
-    l = Node(frequencies[0].chr)
-    r = Node(frequencies[1].chr)
-    p = Node(frequencies[0].frequency + frequencies[1].frequency)
-    l.parent = p
-    r.parent = p
-    frequencies = [p] + frequencies[2:]
-    print(frequencies)
+    while len(frequencies) > 1:
+        frequencies.sort(key=lambda c: c.frequency)
+        p = Node()
+        p.set_children(frequencies[0], frequencies[1])
+        frequencies = [p] + frequencies[2:]
+        print(frequencies)
