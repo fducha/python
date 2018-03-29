18d99e6948995cf757beb5985b739fbaa63d6c21
fducha <fducha@list.ru>
2018-03-29 18:33:09 +0300

    step 13 is fixed but don't work

--- stepic/Algorhythms/step13.py
+++ stepic/Algorhythms/step13.py
@@ -1,5 +1,5 @@
 def merge(lst_l, lst_r):
-    max_len = max(len(lst_l), len(lst_r))
+    max_len = len(lst_l) + len(lst_r)
     result = []
     a, b = lst_l[0], lst_r[0]
     for i in range(max_len):
@@ -9,18 +9,23 @@ def merge(lst_l, lst_r):
         else:
             result.append(b)
             lst_r = lst_r[1:]
+        # FIX IT
+        a, b = lst_l[0], lst_r[0]
     return result
 
 
-def merge_sort(lst, l, r):
-    if l < r:
-        m = (l + r) // 2
-        return merge(merge_sort(lst, l, m), merge_sort(lst, m + 1, r))
+def merge_sort(lst):
+    result = []
+    for i in lst:
+        result.append([i])
+    while len(result) > 1:
+        result.append(merge(result.pop(0), result.pop(0)))
+    return result
 
 
 a = [7, 2, 5, 3, 7, 13, 1, 6]
-s = merge_sort(a, 0, 7)
-print(*s)
+s = merge_sort(a)
+print(s)
 
 # n, nums = int(input()), list(map(int, input().split()))
 
